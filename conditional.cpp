da0d8c39b27599c951032e689a8274f87b0887c9
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-03-10 11:10:26 +0700

    menambahkan inputdata

--- conditional.cpp
+++ conditional.cpp
@@ -2,3 +2,10 @@
 #include <string>
 using namespace std;
 
+void inputData (float &berat,float &tinggi){
+    cout<< "masukan berat badan = ";
+    cin>> berat;
+    cout<< "masukan tinggi badan = ";
+    cin>> tinggi;
+}
+
