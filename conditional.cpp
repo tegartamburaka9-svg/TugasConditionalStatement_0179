d3b10163b35d96c569dcba9e7ddbe22f0123ea7b
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-03-10 11:19:39 +0700

    menambahkan berat badan normal

--- conditional.cpp
+++ conditional.cpp
@@ -16,4 +16,6 @@ float hitungBMI(float berat,float tinggi){
 string Status_BMI(float BMI){
     if (BMI < 18.5)
         return "berat badan kurang";
-    
+    else if(BMI < 25)
+        return "berat badan normal";
+}
